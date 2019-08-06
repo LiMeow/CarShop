4c8642fcc651b1217a369cd881a44f50415f8a6b
Black <cactuscatoo@gmail.com>
2019-08-06 12:24:59 +0600

    db script has been modified

--- sql/carShopDBScript.sql
+++ sql/carShopDBScript.sql
@@ -1,104 +1,50 @@
-
-create type user_role as enum ('user','manager');
+drop schema  public cascade;
+create schema public;
 
 create type transaction_status as enum (
 'application confirmation',
 'test-drive',
 'contract preparation',
 'payment expected',
-'car delivery'); 
+'car delivery',
+'rejected'); 
 
 
-CREATE TABLE user_info( 	
+CREATE TABLE manager ( 	
 	id serial primary key,
-	firstname text not null,
-	patronymic text default null,
-	lastname text not null,
-	email text unique not null,
-	password text not null,
-	role user_role not null
-);
-
-CREATE TABLE brand( 	
-	id serial primary key,
-	brand text not null
-);
-
-CREATE TABLE car_class( 	
-	id serial primary key,
-	class text not null
-);
-
-CREATE TABLE body_type( 	
-	id serial primary key,
-	type text not null
-);
-
-CREATE TABLE fuel( 	
-	id serial primary key,
-	type text not null
-);
-
-CREATE TABLE drive( 	
-	id serial primary key,
-	type text not null
-);
-
-CREATE TABLE engine_type( 	
-	id serial primary key,
-	type text not null
+	username text unique not null,
+	password text not null
 );
 
  CREATE table car (
  id serial primary key,
-
- /*GENERAL*/
- brand_id integer not null REFERENCES brand (id) on delete cascade,
+ picture text not null,
  model text not null,
  price integer not null,
- production integer not null,
- car_class_id integer REFERENCES car_class (id),
- body_type_id integer REFERENCES body_type (id),
- seats integer not null,
- doors integer not null,
- color text not null,
- 
-  /*DRIVE*/
- drive_id integer REFERENCES drive (id),
- engine_type_id integer REFERENCES engine_type (id),
- fuel_id integer REFERENCES fuel (id),
- 
- /*ENGINE*/
- cylinders integer not null,
- valves integer not null,
- cylinder_capacity real not null,
- cylinder_stroke real not null,
- cylinder_bore real not null,
- engine_power real not null,
- engine_torque real not null,
- compression_ration real not null,
- tank_capacity integer not null,
- 
- /*PERFORMANCE*/
- top_speed integer not null,
- acceleration real not null,
-
- /*DIMENSIONS*/
- lenght integer not null,
- width integer not null,
- height integer not null,
- wheelbase integer not null,
- 
+ production integer not null,  
  available boolean not null default true
  );
 
-CREATE TABLE sales_transaction( 	
+CREATE TABLE request ( 	
 	id serial primary key,
-	manager_id integer not null REFERENCES user_info (id),
-	user_id integer not null REFERENCES user_info (id),
+	name text not null,
+	phone text default null,
+	email text default null
+);
+
+CREATE TABLE car_request ( 	
+	id serial primary key,
 	car_id integer not null REFERENCES car (id),
-	status transaction_status not null,
-	updated_at timestamptz not null default now()
+	request_id integer not null REFERENCES request (id),
+    manager_id integer default null REFERENCES manager (id)
+);
+
+CREATE TABLE status( 	
+	id serial primary key,
+	date timestamptz not null default now(),
+	status_name transaction_status not null,
+	description text default null,
+	car_request_id integer not null REFERENCES car_request (id)
 );
 
 
